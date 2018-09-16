a4e97ab7f181eae0167b606600cd69d1aff21768
linjie <xulinjie@gmail.com>
2018-09-16 09:53:31 +0800

    添加测试文件

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/16 上午9:51
+# @Author  : linjie
+# @File    : test.py
+# @Des     : 测试
+
+if __name__ == '__main__':
+    print('hello')
+
+
+
